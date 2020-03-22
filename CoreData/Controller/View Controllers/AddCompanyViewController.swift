ae1ba2db7457ef98ee8da3b19eb9a9aee7004dc1
Kenny Dubroff <kennydubroff@hazystudios.com>
2020-03-21 17:11:40 -0700

    Refactored Delegates

--- CoreData/Controller/View Controllers/AddCompanyViewController.swift
+++ CoreData/Controller/View Controllers/AddCompanyViewController.swift
@@ -8,6 +8,10 @@
 
 import UIKit
 
+protocol AddCompanyDelegate: class {
+    func addCompany(_ company: Company)
+}
+
 class AddCompanyViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
@@ -22,6 +26,7 @@ class AddCompanyViewController: UIViewController {
         self.title = "Add Company"
         view.backgroundColor = .tableViewBackgroundColor
         setupBackButton()
+        setupTextFields()
     }
 
     func setupBackButton() {
@@ -29,4 +34,24 @@ class AddCompanyViewController: UIViewController {
         navigationController?.navigationBar.topItem?.backBarButtonItem = backItem
     }
 
+    func setupTextFields() {
+        let hStackFrame = CGRect(x: 20, y: 40, width: view.frame.size.width - 40, height: view.frame.size.height / 3)
+        //Horizontal Stack
+        let hStack = UIStackView(frame: hStackFrame)
+        hStack.axis = .horizontal
+        hStack.alignment = .fill
+        hStack.distribution = .fill
+        hStack.translatesAutoresizingMaskIntoConstraints = false
+        var frame = CGRect(x: hStack.frame.minX + 20,
+                           y: 20,
+                           width: hStack.frame.width - 40,
+                           height: 40
+        )
+        //add textFields
+        let companyNameField = UITextField(frame: frame)
+        companyNameField.placeholder = "Enter a Company Name"
+        hStack.addArrangedSubview(companyNameField)
+        
+        view.addSubview(hStack)
+    }
 }
